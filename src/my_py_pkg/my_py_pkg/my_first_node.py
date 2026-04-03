26696762c6f3efbcaec9a63a287f4c45ab6b34f6
Akhil R A <197573077+akhilranand@users.noreply.github.com>
2026-04-03 10:46:52 +0530

    node using basic oop

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -2,14 +2,17 @@
 import rclpy
 from rclpy.node import Node
 
+class MyNode(Node):
+    def __init__(self):
+        super().__init__("py_test")
+        self.get_logger().info("haloo world")
 
 def main(args = None):
     rclpy.init(args=args)
     
-    node = Node("py_test")
-    node.get_logger().info("haloo world")
+    node = MyNode()
+    
     rclpy.spin(node)
-
     rclpy.shutdown()
 
 if __name__ == "__main__":
