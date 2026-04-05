46b5fe6c3944bbed47b2d2b6d86cdc3bbd760bfb
Akhil R Anand <197573077+akhilranand@users.noreply.github.com>
2026-04-05 08:52:31 +0530

    Refactor MyNode class and update timer functionality

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,29 +1,82 @@
+// #include "rclcpp/rclcpp.hpp"
+
+
+// class MyNode : public rclcpp::Node{
+//     public:
+//         MyNode() : Node("cpp_test"),counter_(0){
+//              RCLCPP_INFO(this->get_logger(),"hallo world");
+//              timer_ = this->create_wall_timer(std::chrono::seconds(1),
+//                                               std::bind(&MyNode::timerCallback , this ));
+//         }
+//     private:
+//     void timerCallback(){
+//         RCLCPP_INFO(this->get_logger(),"helo %d",counter_);
+//         counter_ ++;
+
+//     }
+//         rclcpp::TimerBase::SharedPtr timer_;
+//         int counter_;
+// };
+
+
+
+// int main(int argc ,char **argv){
+//     rclcpp::init(argc , argv);
+//     auto node = std::make_shared<MyNode>();
+//     rclcpp::spin(node);
+//     rclcpp::shutdown(); 
+//     return 0;
+// }
+
+
+
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode : public rclcpp::Node
+{
+public:
+    // Constructor
+    MyNode() : Node("cpp_test")
+    {
+        count = 0;
 
-class MyNode : public rclcpp::Node{
-    public:
-        MyNode() : Node("cpp_test"),counter_(0){
-             RCLCPP_INFO(this->get_logger(),"hallo world");
-             timer_ = this->create_wall_timer(std::chrono::seconds(1),
-                                              std::bind(&MyNode::timerCallback , this ));
-        }
-    private:
-    void timerCallback(){
-        RCLCPP_INFO(this->get_logger(),"helo %d",counter_);
-        counter_ ++;
+        RCLCPP_INFO(this->get_logger(), "Hello world");
 
+        timer = this->create_wall_timer(
+            std::chrono::seconds(1),
+            std::bind(&MyNode::sayHello, this)
+        );
     }
-        rclcpp::TimerBase::SharedPtr timer_;
-        int counter_;
+
+private:
+    // Member function
+    void sayHello()
+    {
+        RCLCPP_INFO(this->get_logger(), "Count = %d", count);
+        count++;
+    }
+
+    // Data members
+    int count;
+    rclcpp::TimerBase::SharedPtr timer;
 };
 
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
 
-
-int main(int argc ,char **argv){
-    rclcpp::init(argc , argv);
     auto node = std::make_shared<MyNode>();
+
     rclcpp::spin(node);
-    rclcpp::shutdown(); 
+
+    rclcpp::shutdown();
     return 0;
 }
+
+
+
+
+
+
+
+
